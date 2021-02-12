d1c7869709602c6b92fa03b33e1c5ee16e1f7a69
Michael Osthege <m.osthege@fz-juelich.de>
2021-02-12 19:25:37 +0100

    Move tests into the package

--- pyhagelkorn/hagelkorn/tests.py
+++ pyhagelkorn/hagelkorn/tests.py
@@ -2,38 +2,37 @@ import datetime
 import timeit
 import unittest
 
-import hagelkorn
-import hagelkorn.core
+from . import core
 
 
 class TestHagelkorn(unittest.TestCase):
     def test_key_length(self):
 
-        D, K, T = hagelkorn.core.key_length(
-            overflow_years=1, resolution=hagelkorn.Resolution.days, B=10
+        D, K, T = core.key_length(
+            overflow_years=1, resolution=core.Resolution.days, B=10
         )
         assert D == 3
         assert K == 10 ** D
-        assert T < hagelkorn.Resolution.days
+        assert T < core.Resolution.days
 
-        D, K, T = hagelkorn.core.key_length(
-            overflow_years=30, resolution=hagelkorn.Resolution.days, B=27
+        D, K, T = core.key_length(
+            overflow_years=30, resolution=core.Resolution.days, B=27
         )
         assert K == 27 ** D
-        assert T < hagelkorn.Resolution.days
+        assert T < core.Resolution.days
 
         pass
 
     def test_base(self):
-        assert hagelkorn.core.base(9, "0123456789ABCDEF", 2) == "09"
-        assert hagelkorn.core.base(13, "0123456789ABCDEF", 2) == "0D"
-        assert hagelkorn.core.base(16, "0123456789ABCDEF", 2) == "10"
-        assert hagelkorn.core.base(256, "0123456789ABCDEF", 2) == "100"
+        assert core.base(9, "0123456789ABCDEF", 2) == "09"
+        assert core.base(13, "0123456789ABCDEF", 2) == "0D"
+        assert core.base(16, "0123456789ABCDEF", 2) == "10"
+        assert core.base(256, "0123456789ABCDEF", 2) == "100"
         pass
 
     def test_monotonic(self):
-        id = hagelkorn.monotonic(
-            resolution=hagelkorn.Resolution.days,
+        id = core.monotonic(
+            resolution=core.Resolution.days,
             now=datetime.datetime(2018, 12, 31, 23, 59, 59),
             alphabet="0123456789",
             start=datetime.datetime(2018, 1, 1),
@@ -44,22 +43,22 @@ class TestHagelkorn(unittest.TestCase):
         pass
 
     def test_equivalence(self):
-        hs = hagelkorn.HagelSource()
+        hs = core.HagelSource()
         now = datetime.datetime.now()
 
-        assert hs.monotonic(now) == hagelkorn.monotonic(now=now)
+        assert hs.monotonic(now) == core.monotonic(now=now)
 
         pass
 
     def test_random(self):
-        ids = [hagelkorn.random() for i in range(100)]
+        ids = [core.random() for i in range(100)]
         assert len(set(ids)) == 100
         pass
 
 
 class TestHagelSource(unittest.TestCase):
     def test_init(self):
-        hs = hagelkorn.HagelSource(overflow_years=42)
+        hs = core.HagelSource(overflow_years=42)
         assert (hs.end - hs.start).total_seconds() == hs.total_seconds
         assert hs.total_seconds == 42 * 31536000
         assert hs.B == len(hs.alphabet)
@@ -68,8 +67,8 @@ class TestHagelSource(unittest.TestCase):
         pass
 
     def test_monotonic(self):
-        hs = hagelkorn.HagelSource(
-            resolution=hagelkorn.Resolution.days,
+        hs = core.HagelSource(
+            resolution=core.Resolution.days,
             alphabet="0123456789",
             start=datetime.datetime(2018, 1, 1),
             overflow_years=1,
@@ -81,7 +80,7 @@ class TestHagelSource(unittest.TestCase):
         pass
 
     def test_t_0(self):
-        hs = hagelkorn.HagelSource()
+        hs = core.HagelSource()
         first = hs.monotonic(now=hs.start)
         assert set(first) == set(hs.alphabet[0]), (
             "ID at start time was expected to be all {} but"
@@ -90,7 +89,7 @@ class TestHagelSource(unittest.TestCase):
         pass
 
     def test_t_overflow(self):
-        hs = hagelkorn.HagelSource()
+        hs = core.HagelSource()
         overflow = hs.monotonic(now=hs.end)
 
         assert len(overflow) == hs.digits + 1
@@ -100,11 +99,11 @@ class TestHagelSource(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    hs = hagelkorn.HagelSource()
+    hs = core.HagelSource()
     p_obj = min(timeit.Timer(hs.monotonic).repeat(number=100000)) / 100000
-    p_fn = min(timeit.Timer(hagelkorn.monotonic).repeat(number=100000)) / 100000
-    p_rnd = min(timeit.Timer(hagelkorn.random).repeat(number=100000)) / 100000
+    p_fn = min(timeit.Timer(core.monotonic).repeat(number=100000)) / 100000
+    p_rnd = min(timeit.Timer(core.random).repeat(number=100000)) / 100000
     print("hs.monotonic() takes {} µs per call".format(p_obj * 1000 * 1000))
-    print("hagelkorn.monotonic() takes {} µs per call".format(p_fn * 1000 * 1000))
-    print("hagelkorn.random() takes {} µs per call".format(p_rnd * 1000 * 1000))
+    print("monotonic() takes {} µs per call".format(p_fn * 1000 * 1000))
+    print("random() takes {} µs per call".format(p_rnd * 1000 * 1000))
     unittest.main()
